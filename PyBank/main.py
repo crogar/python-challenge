66a3bbbfac099881d4b47bdc3a9aa92fefdac414
crogar <58400421+crogar@users.noreply.github.com>
2021-02-05 14:34:18 -0500

    modified main.py
    Renamed variable "dataset" to data

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,9 +4,9 @@ from collections import namedtuple
 
 #reference to budget_data in ./Resources/budget_data.cs
 budget_data = os.path.join(os.getcwd(),'Resources/budget_data.csv')
-analysis = os.path.join(os.getcwd(),'Analysis/analysis.txt')
+analysis_output = os.path.join(os.getcwd(),'Analysis/analysis.txt')
 #Declaring variables to store the data from the CSV file
-dataset = []
+data = []
 print_out = []
 months = namedtuple("months","Date profit_losses")  # using namedtuple to keep list organized 
 
@@ -16,14 +16,14 @@ def summation(data_set):
     '''
     return sum([month.profit_losses for month in data_set])
 
-def calc_changes(data_set,mode='average'):
+def calc_changes(data_,mode='average'):
     ''' This function receives a list containing Profit/Losses will add them up
         it will return the the average, max or min based on the chosen mode "averag","GRI", or "GRD"
     '''
     changes = []
-    for i in range(0,len(data_set)-1):
-        change = data_set[i+1].profit_losses- data_set[i].profit_losses 
-        changes.append((data_set[i+1].Date,change))
+    for i in range(0,len(data_)-1):
+        change = data_[i+1].profit_losses- data_[i].profit_losses 
+        changes.append((data_[i+1].Date,change))
     if mode == 'average':
         average_change = sum([x[1] for x in changes]) / len(changes)
         return round(average_change,2)
@@ -39,30 +39,29 @@ def main():
         reader = csv.DictReader(csvfile)
         for row in reader:
             month = months(row['Date'], float(row['Profit/Losses']))  # appending monht of type months(namedTuple)
-            dataset.append(month)
+            data.append(month)
 
     print_out.append("Financial Analysis")
     print_out.append("----------------------------")
     # obtaining The total number of months included in the dataset
-    print_out.append(f"Total Months: {len(dataset)}")
+    print_out.append(f"Total Months: {len(data)}")
     # obtaining The net total amount of "Profit/Losses" over the entire period
-    net_total = summation(dataset)
+    net_total = summation(data)
     print_out.append(f"Total: ${net_total}")
     # Calculates the changes in "Profit/Losses" over the entire period, then returns the average of those changes
-    average_change = calc_changes(dataset, mode='average')
+    average_change = calc_changes(data, mode='average')
     print_out.append(f'Average Change: ${average_change}')
     # Calculates The greatest increase in profits (date and amount) over the entire period
-    GRI = calc_changes(dataset,mode="GRI")    # Receiving a tuple containing the month and the GRI change i.e ('Sep-2016', -665765.0)
+    GRI = calc_changes(data,mode="GRI")    # Receiving a tuple containing the month and the GRI change i.e ('Sep-2016', -665765.0)
     print_out.append(f'Greatest Increase in Profits: {GRI[0]} (${GRI[1]})')
     # calculates The greatest decrease in losses (date and amount) over the entire period
-    GRD = calc_changes(dataset,mode="GRD")    # Receiving a tuple containing the month and the GRI change i.e ('Sep-2016', -665765.0)
+    GRD = calc_changes(data,mode="GRD")    # Receiving a tuple containing the month and the GRI change i.e ('Sep-2016', -665765.0)
     print_out.append(f'Greatest Decrease in Profits: {GRD[0]} (${GRD[1]})')
-    for line in print_out:
-        print(line)
 
-    with open(analysis, 'w') as out_file:  # Generating analysis.txt printing out same result as in the console
+    with open(analysis_output, 'w', newline='') as out_file:  # Generating analysis.txt printing out same result as in the console
         for line in print_out:
-            out_file.write(line + "\n")
+            print(line)
+            out_file.write(line + '\n') # using escape codes \n so this way python will add a new line after wrinting a new row
 
 if __name__ == '__main__':
     main()
