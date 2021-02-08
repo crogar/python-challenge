61741246eb31faa79ead60d4b4277a93685ad7ac
crogar <58400421+crogar@users.noreply.github.com>
2021-02-08 11:38:30 -0500

    update main.py
    Corrected Typos in code

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,34 +11,34 @@ print_out = []
 months = namedtuple("months","Date profit_losses")  # using namedtuple to keep list organized 
 
 def summation(data_set): 
-    ''' This function receives a list containing Profit/Losses will add them up
-        it will return the net total amount using index 1 to locate that data
+    ''' This function receives a list containing Profit/Losses, it will add them up.
+        It will return the net total amount using index 1 to locate that data
     '''
     return sum([month.profit_losses for month in data_set])
 
 def calc_changes(data_,mode='average'):
     ''' This function receives a list containing Profit/Losses will add them up
-        it will return the the average, max or min based on the chosen mode "averag","GRI", or "GRD"
+        it will return the the average, max or min based on the chosen mode "average","GRI", or "GRD"
     '''
     changes = []
     for i in range(0,len(data_)-1):
         change = data_[i+1].profit_losses- data_[i].profit_losses 
-        changes.append((data_[i+1].Date,change))
+        changes.append((data_[i+1].Date,change))   # first Element in tuple is the month and second element is the actual change
     if mode == 'average':
         average_change = sum([x[1] for x in changes]) / len(changes)
         return round(average_change,2)
     elif  mode == 'GRI':
-        return max(changes, key = lambda i: i[1])
+        return max(changes, key = lambda i: i[1])  # using Lambda function to tell MAX to use The second element in tuple as comparison 
     elif mode == 'GRD':
-        return min (changes, key = lambda i: i[1])
+        return min (changes, key = lambda i: i[1]) # using Lambda function to tell MAX to use The second element in tuple as comparison 
     else:
-        raise ValueError(f"{mode} is not a supported parameter")
+        raise ValueError(f"{mode} is not a supported parameter") # if user doesn't provide a valid mode, we raise a ValueError
     
 def main():
     with open(budget_data, newline='') as csvfile: # reading CSV File and storing the rows into a list
         reader = csv.DictReader(csvfile)
         for row in reader:
-            month = months(row['Date'], float(row['Profit/Losses']))  # appending monht of type months(namedTuple)
+            month = months(row['Date'], float(row['Profit/Losses']))  # appending month of type months(namedTuple)
             data.append(month)
 
     print_out.append("Financial Analysis")
@@ -55,7 +55,7 @@ def main():
     GRI = calc_changes(data,mode="GRI")    # Receiving a tuple containing the month and the GRI change i.e ('Sep-2016', -665765.0)
     print_out.append(f'Greatest Increase in Profits: {GRI[0]} (${GRI[1]})')
     # calculates The greatest decrease in losses (date and amount) over the entire period
-    GRD = calc_changes(data,mode="GRD")    # Receiving a tuple containing the month and the GRI change i.e ('Sep-2016', -665765.0)
+    GRD = calc_changes(data,mode="GRD")    # Receiving a tuple containing the month and the GRD change i.e ('Sep-2016', -665765.0)
     print_out.append(f'Greatest Decrease in Profits: {GRD[0]} (${GRD[1]})')
 
     with open(analysis_output, 'w', newline='') as out_file:  # Generating analysis.txt printing out same result as in the console
