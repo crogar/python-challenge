4e91defa42473425061abd4c21cbbbddb9591d8b
crogar <58400421+crogar@users.noreply.github.com>
2021-02-11 16:42:14 -0500

    update main.py
    Comments where cleaned up

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,18 +2,17 @@ import os, csv
 from collections import defaultdict,namedtuple
 
 election_data = os.path.join(os.getcwd(),'Resources/election_data.csv') #reference to budget_data in ./Resources/budget_data.cs
-analysis = os.path.join(os.getcwd(),'Analysis/analysis.txt')
+analysis = os.path.join(os.getcwd(),'Analysis/analysis.txt')  # output path.
 #Declaring variables to store the data from the CSV file
 print_out = []
 candidates = defaultdict(lambda: 0)  # using defaultdict allows to count the votes for each candidate without having to worry about the initial value
-votes = namedtuple("votes","Voter_ID County Name")  # using namedtuple to keep list organized 
 
 def analysis_dataset(candidates):
     print_out.append("Election Results\n-------------------------")        
     # sorting Dict out based on Votes 
     candidates = dict(sorted(candidates.items(),key=lambda i: i[1], reverse=True))
     # The total number of votes cast
-    total_votes = sum([votes for votes in candidates.values()])
+    total_votes = sum([votes for votes in candidates.values()]) # where Values() represent total votes received for each candidate
     print_out.append(f"Total Votes: {total_votes}\n-------------------------")
     for k,v in candidates.items():
         # The percentage of votes each candidate won
