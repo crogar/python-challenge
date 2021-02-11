920a77b25bf78f51780eafe4b7987f2c1dda14da
crogar <58400421+crogar@users.noreply.github.com>
2021-02-11 12:06:49 -0500

    update main.py
    modified print_out format

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,5 +1,4 @@
-import os
-import csv
+import os, csv
 from collections import defaultdict,namedtuple
 
 #reference to budget_data in ./Resources/budget_data.cs
@@ -11,33 +10,27 @@ print_out = []
 votes = namedtuple("votes","Voter_ID County Name")  # using namedtuple to keep list organized 
 
 def analysis_dataset():
-    candidates = defaultdict(lambda: 0)
-    print_out.append("Election Results")        # using defaultdict allows to count the votes for each candidate without having to worry about the initial value
-    print_out.append('-------------------------')    
+    candidates = defaultdict(lambda: 0)  # using defaultdict allows to count the votes for each candidate without having to worry about the initial value
+    print_out.append("Election Results\n-------------------------")        
     for candidate in dataset:
         candidates[candidate.Name] += 1
     # sorting Dict out based on Votes 
     candidates = dict(sorted(candidates.items(),key=lambda i: i[1], reverse=True))
     # The total number of votes cast
     total_votes = len(dataset)
-    print_out.append(f"Total Votes: {total_votes}")
-    print_out.append('-------------------------')
+    print_out.append(f"Total Votes: {total_votes}\n-------------------------")
     for k,v in candidates.items():
         # The percentage of votes each candidate won
         percentage = "{0:.3f}%".format((v/total_votes)*100)
         print_out.append(f"{k}: {percentage} ({v})")
-    print_out.append('-------------------------')      
-    # The winner of the election based on popular vote. 
-    # using nex(iter()) to get the first candidate in the dict, this because we previously sorted it!!!
-    print_out.append(f"Winner: {next(iter(candidates.keys()))}") 
-    print_out.append('-------------------------')     
-    for line in print_out:
-        print(line)
+    # The winner of the election based on popular vote.
+    winner = next(iter(candidates.keys())) # using nex(iter()) to get the first candidate in the dict, this because we previously sorted it out!!!
+    print_out.append(f"-------------------------\nWinner: {winner}\n-------------------------")  
     # exports a text file with the results.
     with open(analysis, 'w+') as out_file:  # Generating analysis.txt printing out same result as in the console
         for line in print_out:
-            line = line + "\n"
-            out_file.write(line)
+            print(line)
+            out_file.write(line + '\n')
 
 def main():
     with open(election_data, newline='') as csvfile: # reading CSV File and storing the rows into a list
